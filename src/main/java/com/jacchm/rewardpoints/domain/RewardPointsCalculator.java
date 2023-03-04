472298b2e4d05abfefd8e5cde0a98ae73c7f7b18
jacchm <jacekchmiel03@gmail.com>
2023-03-04 23:23:47 +0100

    rewrite reward points calculator

--- src/main/java/com/jacchm/rewardpoints/domain/RewardPointsCalculator.java
+++ src/main/java/com/jacchm/rewardpoints/domain/RewardPointsCalculator.java
@@ -1,30 +1,51 @@
 package com.jacchm.rewardpoints.domain;
 
 import com.jacchm.rewardpoints.domain.model.Transaction;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
-import java.util.Map;
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RewardPointsCalculator {
 
-  private static final Map<Integer, Integer> REWARD_POINTS_TIERS = Map.of(
-      50, 0,
-      100, 2,
-      Integer.MAX_VALUE, 1);
+  private static final List<Tuple2<Integer, Integer>> REWARD_POINTS_TIERS = new ArrayList<>();
+
+  @PostConstruct
+  void setup() {
+    REWARD_POINTS_TIERS.add(Tuples.of(0, 0));
+    REWARD_POINTS_TIERS.add(Tuples.of(50, 1));
+    REWARD_POINTS_TIERS.add(Tuples.of(100, 2));
+  }
 
   public Integer calculateRewardPointsPerTransaction(final Transaction transaction) {
-    int numberOfRewardPoints = 0;
-    int paymentRounded = transaction.getAmount().intValue();
-    if (paymentRounded <= 50) {
-      return 0;
+    int payment = transaction.getAmount().intValue();
+    int tierNo = findMaximalApplicableTier(payment);
+    return calculateRewardPoint(payment, tierNo);
+  }
+
+  private int findMaximalApplicableTier(final int payment) {
+    int tierNo = 0;
+    for (int i = 0; i < REWARD_POINTS_TIERS.size(); i++) {
+      if (payment > REWARD_POINTS_TIERS.get(i).getT1()) {
+        tierNo = i;
+      }
     }
-    if (paymentRounded > 100) {
-      numberOfRewardPoints += (paymentRounded - 100) * 2;
-      paymentRounded -= paymentRounded - 100;
+    return tierNo;
+  }
+
+  private int calculateRewardPoint(int payment, final int tierNo) {
+    int rewardPoints = 0;
+    Tuple2<Integer, Integer> tier;
+    int amountInTier;
+    for (int i = tierNo; i >= 0; i--) {
+      tier = REWARD_POINTS_TIERS.get(i);
+      amountInTier = payment - tier.getT1();
+      rewardPoints += amountInTier * tier.getT2();
+      payment -= amountInTier;
     }
-    if (paymentRounded <= 100) {
-      numberOfRewardPoints += (paymentRounded - 50);
-    }
-    return numberOfRewardPoints;
+    return rewardPoints;
   }
 
 }
